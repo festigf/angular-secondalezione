f35bfa614bb036a3beb0dd015fc6eb747e00ae3c
festigf <gianfranco.festi@marconirovereto.it>
2020-01-21 19:24:59 +0100

    1 file modified

--- src/books.service.ts
+++ src/books.service.ts
@@ -15,8 +15,8 @@ export class BooksService {
     let header: HttpHeaders = new HttpHeaders();
 
 //header = header.set('Authorization', ('Token ' + auth));
-header = header.set('Accept'," text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-header = header.append("Content-Type", "text/plain; charset=utf-8");
+//header = header.set('Accept'," text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+//header = header.append("Content-Type", "text/plain; charset=utf-8");
 
     return this.http
     .get('http://festigf.pythonanywhere.com/getbook/1',{ headers: header })
