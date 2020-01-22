c66b6bcbb5151bc53ceeda460400510d770ecb6e
festigf <gianfranco.festi@marconirovereto.it>
2020-01-22 08:38:47 +0100

    1 file modified

--- src/books.service.ts
+++ src/books.service.ts
@@ -22,8 +22,8 @@ export class BooksService {
 console.log(btoa('festigf:AltaFormazione'));
     let header: HttpHeaders = new HttpHeaders();
 
-//header = header.set('Authorization', 'Basic ' + btoa('festigf:AltaFormazione2020'));
-header = header.set('authorization', 'Basic ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA=');
+header = header.set('Authorization', 'Basic ' + btoa('festigf:AltaFormazione2020'));
+//header = header.set('authorization', 'Basic ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA=');
 /*header.append("cache-control", "no-cache");
 header.append("Content-Type", "application/json");
 header.append('Access-Control-Allow-Headers', 'Content-Type');
