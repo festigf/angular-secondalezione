5db62feb371634a084365dcd8e4b02c5f9281cb4
festigf <gianfranco.festi@marconirovereto.it>
2020-01-22 08:33:24 +0100

    1 file modified

--- src/books.service.ts
+++ src/books.service.ts
@@ -13,29 +13,26 @@ export class BooksService {
 
   getBooks(){
 
-    const httpOptions = {
+    /*const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
     'Authorization': 'Basic ' + btoa('festigf:AltaFormazione')
   })
-};
-  //  let header: HttpHeaders = new HttpHeaders();
+};*/
+console.log(btoa('festigf:AltaFormazione'));
+    let header: HttpHeaders = new HttpHeaders();
 
-//header = header.set('Authorization', ('Basic ' + 'ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA='));
-
-/*header.append("Content-Type", "application/json");
+//header = header.set('Authorization', 'Basic ' + btoa('festigf:AltaFormazione2020'));
+header = header.set('authorization', 'Basic ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA=');
+header.append("cache-control", "no-cache");
+header.append("Content-Type", "application/json");
 header.append('Access-Control-Allow-Headers', 'Content-Type');
 header.append('Access-Control-Allow-Methods', 'GET');
 header.append('Access-Control-Allow-Origin', '*');
-*/
-/*{
-      
-      headers: header
-      
 
-    } */
+/*{headers: header} */
     return this.http
-    .get('https://festigf.pythonanywhere.com/books',httpOptions)
+    .get('https://festigf.pythonanywhere.com/books',{headers: header})
     .pipe(catchError(err => {
       console.log(err)
       return throwError(err);
