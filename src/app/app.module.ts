d6e02886f678318cccc86e4405bfb785a65622f4
festigf <gianfranco.festi@marconirovereto.it>
2020-01-23 11:00:58 +0100

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule,HTTP_INTERCEPTORS } from '@angular/common/http';
+import {CustomInterceptor} from './custominterceptor';
+
 import { BooksService} from '../books.service';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +12,11 @@ import { ShowbooksComponent } from './showbooks/showbooks.component';
 @NgModule({
   imports:      [ BrowserModule, FormsModule,HttpClientModule ],
   declarations: [ AppComponent, HelloComponent, ShowbooksComponent ],
-  providers:    [ BooksService ],
+  providers:    [ BooksService,{
+      provide: HTTP_INTERCEPTORS,
+      useClass: CustomInterceptor ,
+      multi: true
+    } ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
