e684bde5d6f6396733d18b96cb62cafc25999c64
festigf <gianfranco.festi@marconirovereto.it>
2020-01-24 11:11:30 +0100

    1 file modified

--- src/app/custominterceptor.ts
+++ src/app/custominterceptor.ts
@@ -7,12 +7,13 @@ export class CustomInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log("interceptor")
+        //'Content-Type': 'application/json; charset=utf-8',
        const r = request.clone({
           
             withCredentials: true,
             setHeaders: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Access-Control-Allow-Credentials': 'true',
+                
+                'Access-Control-Allow-Credentials': `true`,
                 Authorization: `Basic ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA=`,
                 
                 'Access-Control-Allow-Origin': '//localhost:4200',
