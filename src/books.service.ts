8ccc19d0e2b8acf1a25d1a4f82756840c075e6ff
festigf <gianfranco.festi@marconirovereto.it>
2020-01-24 11:51:53 +0100

    1 file modified

--- src/books.service.ts
+++ src/books.service.ts
@@ -59,7 +59,8 @@ header.append('Authorization','Basic ZmVzdGlnZjpBbHRhRm9ybWF6aW9uZTIwMjA=');
 /*{headers: header} 
 {headers: header, withCredentials: true}*/
     return this.http
-    .get('https://festigf.pythonanywhere.com/api/books'); //,httpOptions);/*
+    //.get('https://festigf.pythonanywhere.com/api/books'); //,httpOptions);/*
+    .get('https://festigf.pythonanywhere.com/'); 
    /* .pipe(catchError(err => {
       console.log(err);
       //return this.http
